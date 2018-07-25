a196d84a5922720472ba3a59465e053a60933d6e
abhishekghoshal <40827090+abhishekghoshal@users.noreply.github.com>
2018-07-25 15:59:29 +0530

    text added

---
+++ PlotDemo.R
@@ -0,0 +1,4 @@
+x<- c(2,5,7,8,10,15,20)
+y<- c(10, 15, 17, 19, 25, 40, 55)
+plot (x,y)
+plot (x, y, xlab = 'rubbish', ylab = 'garbage')
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # workshop2
 demo2 for workshop
+
+new branch with major change
